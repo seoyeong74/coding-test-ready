d8346ccc44ebac04560082724e889bdaba670105
seoyeong74 <sychoy010704@gmail.com>
2024-03-04 16:34:48 +0900

    파일 변경

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,76 +1,63 @@
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main
 {
-    static int result = Integer.MIN_VALUE;;
-    static boolean[] learnCharList = new boolean[26];
-    static ArrayList<String> wordList = new ArrayList<>();
+    static double[] probabilityList = new double[4];
+    static int N;
+    static double result = 0.0;
 
-    // tip: arguments are passed via the field below this editor
     public static void main(String[] args) throws Exception
     {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        int N = Integer.parseInt(st.nextToken()), K = Integer.parseInt(st.nextToken());
-        if(K < 5){
-            System.out.println("0");
-            return;
-        }
-        else if(K == 26){
-            System.out.println(N);
-            return;
+        N = Integer.parseInt(st.nextToken());
+
+        for(int i = 0; i < 4; i++){
+            probabilityList[i] = Integer.parseInt(st.nextToken()) / 100.0;
         }
 
-        learnCharList[('a' - 'a')] = true;
-        learnCharList[('n' - 'a')] = true;
-        learnCharList[('t' - 'a')] = true;
-        learnCharList[('i' - 'a')] = true;
-        learnCharList[('c' - 'a')] = true;
+        boolean[][] visited = new boolean[29][29];
 
-        
-        //HashSet<Character> toLearnChar = new HashSet<>();
+        int initX = 14, initY = 14;
+        visited[initX][initY] = true;
+        bfs(0, 1.0, initX, initY, visited);
 
-        for(int i = 0; i< N; i++){
-            String inputString = br.readLine().replaceAll("a|n|t|i|c", "");
-            wordList.add(inputString);
-        }
-
-        bfs(K - 5, 0);
+        System.out.println(result);
 
         br.close();
-        System.out.println(result);
     }
 
-    static void bfs(int ableCharNum, int lastLearnIdx){
+    static void bfs(int moveNum, double prob, int currX, int currY, boolean[][] visited){
 
-        if (ableCharNum == 0){ //배울 알파벳 다 배움
-            int readWordNum = 0;
-            for(String word: wordList){
-                boolean ableRead = true;
-                for(char charc: word.toCharArray()){
-                    ableRead = learnCharList[charc - 'a'];
-                    if(!ableRead)
-                        break;
-                }
-
-                if(ableRead) readWordNum++;
-            }
-
-            result = Math.max(result, readWordNum);
-
+        if(moveNum >= N){
+            result += prob;
             return;
         }
 
-        for(int i = lastLearnIdx; i < 26; i++){
-            if(!learnCharList[i]){
-                learnCharList[i] = true;
-                bfs(ableCharNum - 1, i);
-                learnCharList[i] = false;    
+        for(int i = 0; i < 4; i++){
+            int nextX = currX, nextY = currY;
+            if(i == 0){
+                nextX++;
             }
-            
+            else if(i== 1){
+                nextX--;
+            }
+            else if(i== 2){
+                nextY--;
+            }
+            else if(i==3){
+                nextY++;
+            }
+            double nextPorb = prob * probabilityList[i];
+
+            if (!visited[nextX][nextY]){
+                visited[nextX][nextY] = true;
+                bfs(moveNum + 1, nextPorb, nextX, nextY, visited);
+                visited[nextX][nextY] = false;
+            }
         }
     }
 }
